40bdb5646bb256250a742d8fa18ba9eff43e783e
Yohannes Kassaye <jmule1567@gmail.com>
2025-05-25 09:23:41 -0700

    Add date time formatter

--- DELI-cious/src/main/java/ReceiptManagement.java
+++ DELI-cious/src/main/java/ReceiptManagement.java
@@ -1,5 +1,8 @@
 import java.io.FileWriter;
+import java.text.DateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class ReceiptManagement {
@@ -7,9 +10,10 @@ public class ReceiptManagement {
     public static void writingReceipt(List<Menu> menu){
 
         LocalDateTime localDateTime =  LocalDateTime.now();
-        String date = String.valueOf(localDateTime);
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss ");
+        String date = localDateTime.format(timeFormatter);
 
-        try( FileWriter fileWriter = new FileWriter(date) ) {
+        try( FileWriter fileWriter = new FileWriter(date + ".txt") ) {
             double price = 0;
             for (Menu menu1 : menu){
                 fileWriter.write(menu1.getName());
